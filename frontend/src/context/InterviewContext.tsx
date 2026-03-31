3d885390a8acb16f330a8d98a2c92b16a6b77b6c
Moumita-Mukherjee1234 <mukherjee.moumita899@gmail.com>
2026-03-31 17:16:27 +0530

    changes made

--- frontend/src/context/InterviewContext.tsx
+++ frontend/src/context/InterviewContext.tsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState } from "react";
-import {api} from "../lib/api"
+import api from "../lib/api"
 
 export interface InterviewReport {
   _id?: string;
--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,9 +1,8 @@
-// frontend/src/lib/api.ts
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // ✅ reads .env variable
-  withCredentials: true, // send & receive cookies
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
 });
 
 export default api;
